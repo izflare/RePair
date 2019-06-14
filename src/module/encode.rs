015cc6fd3d23e42cb385041969887d7c218a0edf
flare <@izflare>
2019-06-14 13:06:24 +0900

    [tmp]

--- src/module/encode.rs
+++ src/module/encode.rs
@@ -5,63 +5,78 @@ use bit_vec::BitVec;
 use std::time::Instant;
 use std::cmp::{min, max};
 use strlib::delta;
+use strlib::gamma;
+use strlib::ffenc;
 use super::{cfg::*};
 
 pub fn encode(g: &Grammar, bv: &mut BitVec) -> () {
 
     let start = Instant::now();
 
-    fn u_to_bv(x: u32, logn: u32, bv: &mut BitVec) -> () {
-        let mut z = x;
-        z = z.rotate_right(logn);
-        for _ in 0..logn {
-            z = z.rotate_left(1);
-            bv.push(z % 2 == 1);
-        }
-    }
-    let mut v: Vec<u32> = Vec::new();
+    let mut z = BitVec::new();
+    ffenc::encode(&g.terminal.iter().map(|x| *x as u32).collect::<Vec<u32>>(), &mut z);
 
-    let mut prev = 0;
-    let mut lr = BitVec::new();
-    let mut r: Vec<u32> = Vec::new();
-    let mut u = Vec::new();
-    let mut ren = 1;
-    for e in &g.rule {
-        let m = max(e[0], e[1]);
-        if prev <= m {
-            v.push(m - prev + 1);
-            ren += 1;
+    let mut deltas: Vec<u32> = Vec::new();
+    let mut starting_values: Vec<u32> = Vec::new();
+    let mut starting_points: Vec<u32> = Vec::new();
+    let mut deltas_minimums: Vec<u32> = Vec::new();
+    let mut max_first: BitVec = BitVec::new();
+    let mut last_max: u32 = 0;
+    let mut last_incr_seq: u32 = 0;
+
+    for i in 0..g.rule.len() {
+        let ab = &g.rule[i];
+        let a = max(ab[0], ab[1]);
+        deltas_minimums.push(a - min(ab[0], ab[1]) + 1);
+        max_first.push(a == ab[0]);
+
+        if a >= last_max {
+            deltas.push(a - last_max + 1);
         }
         else {
-            v.push(m);
-            r.push(ren);
-            ren = 1;
+            starting_values.push(a);
+            starting_points.push(i as u32 - last_incr_seq);
+            last_incr_seq = i as u32;
         }
-        lr.push(e[0] >= e[1]);
-        prev = m;
-        u.push(max(e[0], e[1]) - min(e[0], e[1]) + 1);
+        last_max = a;
     }
-    r.push(ren);
-    let sbitlen = std::usize::MAX.count_ones() - (g.terminal.len() + g.rule.len()).leading_zeros();
 
-    u_to_bv(g.terminal.len() as u32, 8, bv);
-    for e in &g.terminal {u_to_bv(*e as u32, 8, bv);}
-    u_to_bv(g.rule.len() as u32, 32, bv);
-    u_to_bv(sbitlen as u32, 32, bv);
-    for e in &u {u_to_bv(*e, sbitlen, bv);}
-    for bit in &lr {bv.push(bit);}
-    u_to_bv(g.sequence.len() as u32, 32, bv);
-    for e in &g.sequence {u_to_bv(*e, sbitlen, bv);}
-    delta::encode(&v, bv);
-    delta::encode(&r, bv);
+    ffenc::to_bits(z.len() as u32, 32, bv);
+    for b in &z {bv.push(b);}
+
+    let mut deltas_bits = BitVec::new();
+    gamma::encode(&deltas, &mut deltas_bits);
+    ffenc::to_bits(deltas_bits.len() as u32, 32, bv);
+    for b in &deltas_bits {bv.push(b);}
+
+    let mut starting_values_bits = BitVec::new();
+    ffenc::encode(&starting_values, &mut starting_values_bits);
+    ffenc::to_bits(starting_values_bits.len() as u32, 32, bv);
+    for b in &starting_values_bits {bv.push(b);}
+
+    let mut starting_points_bits = BitVec::new();
+    gamma::encode(&starting_points, &mut starting_points_bits);
+    ffenc::to_bits(starting_points_bits.len() as u32, 32, bv);
+    for b in &starting_points_bits {bv.push(b);}
+
+    let mut deltas_minimums_bits = BitVec::new();
+    ffenc::encode(&deltas_minimums, &mut deltas_minimums_bits);
+    ffenc::to_bits(deltas_minimums_bits.len() as u32, 32, bv);
+    for b in &deltas_minimums_bits {bv.push(b);}
+
+    ffenc::to_bits(max_first.len() as u32, 32, bv);
+    for b in &max_first {bv.push(b);}
+
+    ffenc::encode(&g.sequence, bv);
+
     let end = start.elapsed();
-
     println!("[Result: bit encoding]");
-    println!("Increasing seq    : {:?}", r.len());
+    println!("Increasing seq    : {:?}", starting_points.len() + 1);
     println!("Bit length        : {:?} [bits]", bv.len());
     println!("{}.{:03} sec elapsed", end.as_secs(), end.subsec_nanos()/1_000_000);
 }
 
+// not impl yet
 pub fn decode(bv: &BitVec, w: &mut Vec<u8>) -> () {
     let mut v: Vec<u32> = Vec::new();
     let mut g: Grammar = Grammar::new();
